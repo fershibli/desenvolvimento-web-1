a833fd356f8dc0c27c3bf06406f1d60e7fe69308
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-26 20:36:51 -0300

    refactor(Projeto08): split logic

--- Projeto08CalculadoraJavascript/script.js
+++ Projeto08CalculadoraJavascript/script.js
@@ -1,6 +1,7 @@
 function somaN1N2() {
-    n1 = document.getElementById('n1').value
-    n2 = document.getElementById('n2').value
-    resposta = document.getElementById('resposta')
-    resposta.innerHTML = "Número 1 + Número 2 = " + (parseInt(n1) + parseInt(n2))
+    elemento_n1 = document.getElementById('n1').value
+    elemento_n2 = document.getElementById('n2').value
+    elemento_resposta = document.getElementById('resposta')
+    resultado = (Number(elemento_n1) + Number(elemento_n2))
+    elemento_resposta.innerHTML = "Número 1 + Número 2 = " + resultado
 }

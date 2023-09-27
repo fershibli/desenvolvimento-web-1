efdefccee890ab0cb4b21655d0f88155ace7b451
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-26 21:57:29 -0300

    feat(Projeto08): multiplicar

--- Projeto08CalculadoraJavascript/script.js
+++ Projeto08CalculadoraJavascript/script.js
@@ -1,7 +1,13 @@
-function somaN1N2() {
-    const elemento_n1 = document.getElementById('n1').value;
-    const elemento_n2 = document.getElementById('n2').value;
-    const elemento_resposta = document.getElementById('resposta');
-    const resultado = (Number(elemento_n1) + Number(elemento_n2));
+const elemento_n1 = document.getElementById('n1');
+const elemento_n2 = document.getElementById('n2');
+const elemento_resposta = document.getElementById('resposta');
+
+function somar() {
+    const resultado = (Number(elemento_n1.value) + Number(elemento_n2.value));
+    elemento_resposta.innerHTML = "Número 1 + Número 2 = " + resultado;
+}
+
+function multiplicar() {
+    const resultado = (Number(elemento_n1.value) * Number(elemento_n2.value));
     elemento_resposta.innerHTML = "Número 1 + Número 2 = " + resultado;
 }

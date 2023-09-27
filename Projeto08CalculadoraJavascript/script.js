71c008f09037c90915a566ac3f15c211a107982c
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-26 21:47:33 -0300

    fix(Projeto08): js var declaration

--- Projeto08CalculadoraJavascript/script.js
+++ Projeto08CalculadoraJavascript/script.js
@@ -1,7 +1,7 @@
 function somaN1N2() {
-    elemento_n1 = document.getElementById('n1').value
-    elemento_n2 = document.getElementById('n2').value
-    elemento_resposta = document.getElementById('resposta')
-    resultado = (Number(elemento_n1) + Number(elemento_n2))
-    elemento_resposta.innerHTML = "Número 1 + Número 2 = " + resultado
+    const elemento_n1 = document.getElementById('n1').value;
+    const elemento_n2 = document.getElementById('n2').value;
+    const elemento_resposta = document.getElementById('resposta');
+    const resultado = (Number(elemento_n1) + Number(elemento_n2));
+    elemento_resposta.innerHTML = "Número 1 + Número 2 = " + resultado;
 }

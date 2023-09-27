36ebcda998330106916e562c3e4897e2cb4eb423
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-26 22:12:37 -0300

    feat(Projeto08): verifica campos vazios

--- Projeto08CalculadoraJavascript/script.js
+++ Projeto08CalculadoraJavascript/script.js
@@ -2,12 +2,25 @@ const elemento_n1 = document.getElementById('n1');
 const elemento_n2 = document.getElementById('n2');
 const elemento_resposta = document.getElementById('resposta');
 
+function verificaVazio() {
+    if (elemento_n1.value == "" || elemento_n2.value == "") {
+        elemento_resposta.innerHTML = "Você esqueceu de preencher todos os campos!"
+        elemento_resposta.style.color = "red"
+        return true
+    } else {
+        elemento_resposta.style.color = "unset"
+    }
+    return false
+}
+
 function somar() {
+    if (verificaVazio()) return;
     const resultado = (Number(elemento_n1.value) + Number(elemento_n2.value));
     elemento_resposta.innerHTML = "Número 1 + Número 2 = " + resultado;
 }
 
 function multiplicar() {
+    if (verificaVazio()) return;
     const resultado = (Number(elemento_n1.value) * Number(elemento_n2.value));
     elemento_resposta.innerHTML = "Número 1 * Número 2 = " + resultado;
 }

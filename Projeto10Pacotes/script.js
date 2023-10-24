72b8602b11079f2cfa9fcc47f74506ef34314ae4
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-10-24 19:48:20 -0300

    feat(Projeto10): search checked

--- Projeto10Pacotes/script.js
+++ Projeto10Pacotes/script.js
@@ -1,15 +1,15 @@
-function calcularDesconto()
-{
+function calcularDesconto() {
     let qtdPacotes = document.querySelectorAll("input[name='pacotes']").length;
     console.log(qtdPacotes);
 
-    let pacotes = document.querySelectorAll("input[name='pacotes']");  
+    let pacotes = document.querySelectorAll("input[name='pacotes']");
     console.log(pacotes);
 
 
-    //            i < pacotes.length
-    for (var i=0; i < qtdPacotes; i++)
-    {
-        console.log(pacotes[i].value);
+    for (var i = 0; i < qtdPacotes; i++) {
+        if (pacotes[i].checked) {
+            console.log(pacotes[i].value);
+            break;
+        }
     }
 }
